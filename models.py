eb77f49dfe89f553d50b81d53dc0787474183e1c
d1124182034-hash <d1124182034@gm.lhu.edu.tw>
2026-02-27 14:56:07 +0800

    Update models.py

--- models.py
+++ models.py
@@ -12,14 +12,14 @@ def get_cloud_models(api_key):
 
         # 精選模型
         keep_models = [
-            "gpt-oss:20b",
             "cogito-2.1:671b",
             "glm-4.6",
             "ministral-3:8b",
             "qwen3-coder:480b",
+            "gpt-oss:20b",
             "minimax-m2"
         ]
-        return [m for m in keep_models if m in models]
+        return [m for m in models if m in keep_models]
 
     except Exception as e:
         st.error(f"取得模型清單失敗: {e}")
