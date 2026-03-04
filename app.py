21bf8d520c774d390fecc193a8085906a7a1a210
d1124182034-hash <d1124182034@gm.lhu.edu.tw>
2026-03-04 21:40:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ from config import init_config
 init_config()
 
 def main():
-    # 確保資料庫有正確的資料表
     if "user" in st.session_state:
         ollama_chat() 
     else:
-        col1, col2, col3 = st.columns([1, 2, 1])
+        # 調整欄位比例，讓中間的登入框稍微收窄，看起來更集中
+        col1, col2, col3 = st.columns([1.2, 2.5, 1.2]) 
         
         with col2:
             st.title("Ollama Cloud")
-            st.subheader("歡迎回來，請先登入您的帳號")
+            
+            # 改用 markdown 替換 subheader，消除討厭的連結圖示，並加上一點底部留白
+            st.markdown("##### 歡迎回來請先登入您的帳號")
+            st.markdown("<br>", unsafe_allow_html=True) 
             
             tab_login, tab_reg = st.tabs(["🔑 帳號登入", "🆕 註冊新帳號"])
             
